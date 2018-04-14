2df908dcbfe1e1173a45e48c2658ad4aa0be6da6
natyur <natalia.yurkovets@gmail.com>
2018-04-14 19:43:58 +0300

    chore(project): update configurators

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -28,9 +28,9 @@ contract Configurator is Ownable {
 
     preITO.setWallet(0xa86780383E35De330918D8e4195D671140A60A74);
     preITO.setStart(1524441600);
-    preITO.addMilestone(10, 20);
-    preITO.addMilestone(10, 15);
-    preITO.addMilestone(10, 10);
+    preITO.addMilestone(10, 200);
+    preITO.addMilestone(10, 150);
+    preITO.addMilestone(10, 100);
     preITO.setHardcap(6282000000000000000000);
 
     token.setSaleAgent(preITO);
@@ -39,13 +39,13 @@ contract Configurator is Ownable {
     ito.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
     ito.setStart(1527206400);
     ito.setPeriod(44);
-    ito.setFirstBonusPercent(5);
-    ito.setFirstBonusLimitPercent(20);
+    ito.setFirstBonusPercent(50);
+    ito.setFirstBonusLimitPercent(200);
     ito.setHardcap(37697000000000000000000);
 
-    ito.addWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085, 15);
-    ito.addWallet(0xa86780383E35De330918D8e4195D671140A60A74, 5);
-    ito.addWallet(0x675eDE27cafc8Bd07bFCDa6fEF6ac25031c74766, 5);
+    ito.addWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085, 150);
+    ito.addWallet(0xa86780383E35De330918D8e4195D671140A60A74, 50);
+    ito.addWallet(0x675eDE27cafc8Bd07bFCDa6fEF6ac25031c74766, 50);
 
     preITO.setNextSaleAgent(ito);
 
@@ -55,7 +55,7 @@ contract Configurator is Ownable {
   }
 
   function commonConfigure(AssembledCommonSale sale) internal {
-    sale.setPercentRate(100);
+    sale.setPercentRate(1000);
     sale.setMinInvestedLimit(100000000000000000);
     sale.setPrice(3184000000000000000000);
     sale.addValueBonus(3000000000000000000, 10);
