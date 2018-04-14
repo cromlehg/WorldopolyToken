1d72d1bdbe8071fce44c42127921355fec00f877
natyur <natalia.yurkovets@gmail.com>
2018-04-14 10:58:31 +0300

    fix(ExtendedWalletsMintTokensFeature): fix summaryTokensPercent counting

--- contracts/ExtendedWalletsMintTokensFeature.sol
+++ contracts/ExtendedWalletsMintTokensFeature.sol
@@ -4,7 +4,7 @@ import './MintTokensInterface.sol';
 import './WalletsPercents.sol';
 //import './PercentRateProvider.sol';
 
-contract ExtendedWalletsMintTokensFeature is /*rcentRateProvider,*/ MintTokensInterface, WalletsPercents {
+contract ExtendedWalletsMintTokensFeature is /*PercentRateProvider,*/ MintTokensInterface, WalletsPercents {
 
   using SafeMath for uint;
 
@@ -13,7 +13,7 @@ contract ExtendedWalletsMintTokensFeature is /*rcentRateProvider,*/ MintTokensIn
   function mintExtendedTokens() public onlyOwner {
     uint summaryTokensPercent = 0;
     for(uint i = 0; i < wallets.length; i++) {
-      summaryTokensPercent.add(percents[wallets[i]]);
+      summaryTokensPercent = summaryTokensPercent.add(percents[wallets[i]]);
     }
     uint mintedTokens = token.totalSupply();
     uint allTokens = mintedTokens.mul(percentRate).div(percentRate.sub(summaryTokensPercent));
